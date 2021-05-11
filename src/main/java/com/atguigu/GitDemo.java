f5c5e316f4bddd190edb514bf54e9b2d94ad7e4b
Czy0923 <churuigongzuo@163.com>
2021-05-11 18:54:49 +0800

    master第二次提交

--- src/main/java/com/atguigu/GitDemo.java
+++ src/main/java/com/atguigu/GitDemo.java
@@ -2,6 +2,7 @@ package com.atguigu;
 
 public class GitDemo {
     public static void main(String[] args) {
-        System.out.println("Hello Git");
+        System.out.println("Hello Git1");
+        System.out.println("Hello Git2");
     }
 }
