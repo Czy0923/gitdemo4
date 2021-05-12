d16a752a17694be0577429986f5cd6ae0a3a5dd3
Czy0923 <52639814+Czy0923@users.noreply.github.com>
2021-05-12 16:31:43 +0800

    master第七次提交

--- src/main/java/com/atguigu/GitDemo.java
+++ src/main/java/com/atguigu/GitDemo.java
@@ -2,7 +2,7 @@ package com.atguigu;
 
 public class GitDemo {
     public static void main(String[] args) {
-        System.out.println("Hello Git1");
+        System.out.println("Hello Git100");
         System.out.println("Hello Git2");
         System.out.println("Hello Git3");
         System.out.println("Hello Git4 + master");
