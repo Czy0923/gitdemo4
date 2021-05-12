7331051d5276f2cf65157ad160d2ef7739015e72
Czy0923 <52639814+Czy0923@users.noreply.github.com>
2021-05-12 16:18:10 +0800

    master第五次提交

--- src/main/java/com/atguigu/GitDemo.java
+++ src/main/java/com/atguigu/GitDemo.java
@@ -9,6 +9,7 @@ public class GitDemo {
         System.out.println("Hello Git5 + hot-fix");
         System.out.println("Hello Git6 + master + hot-fix");
         System.out.println("Hello Git7 + hot-fix");
+        System.out.println("Hello Git8");
 
     }
 }
